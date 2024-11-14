12cf0d0a2ca34b8a4920b17846397fde477d7cee
matthew-saxby <matthew_saxby@taylor.edu>
2024-11-14 15:08:50 -0500

    hello mason. here are my changes. poopy poopy.

---
+++ src/data/load_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+diabetes = pd.read_csv("data/diabetes_prediction_dataset.csv")
+
+
---
+++ src/data/preprocess.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+#load in data
+from load_data import diabetes
+
+#change male and female to 0 and 1
+gender_map = {'Male': 0, 
+              'Female': 1
+              }
+
+diabetes['gender'] = diabetes['gender'].replace(gender_map) 
+
+#change smoking history
+smoking_map = {'No Info':0,
+               'never':1,
+               'ever':2,
+               'former':3,
+               'not current':3,
+               'current':4
+               }
+
+diabetes['smoking_history'] = diabetes['smoking_history'].replace(smoking_map) 
+
+print(diabetes.head())
