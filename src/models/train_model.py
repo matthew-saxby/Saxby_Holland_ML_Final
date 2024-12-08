72d21b12606c4139d89bf05b75d60bb9a2b2386d
matthew-saxby <matthew_saxby@taylor.edu>
2024-12-08 16:46:58 -0500

    yep

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,5 @@
+##### IMPORTS
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -13,72 +15,60 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, confu
 import seaborn as sns
 
 
+##### main data import
 diabetes_processed = pd.read_csv('diabetes_processed.csv')
-
+#establish X and y (features and labels)
 X = diabetes_processed[['gender','age','hypertension','heart_disease','smoking_history','bmi','HbA1c_level','blood_glucose_level']]# Features
 y = diabetes_processed['diabetes']# labels
-
 #set random seed
 np.random.seed(123)
-
 # Split the data into 70% train and 30% validation/test
 X_train, X_test_val, y_train, y_test_val = train_test_split(X, y, test_size=0.3, random_state=123)
 # Split the 70% train/validation into 66% train and 33% validation
 X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=0.66666, random_state=123)
-
-
 #uncomment to see the shape of the data
 #print('Training:', X_train.shape)
 #print('Validation:', X_val.shape)
 #print('Testing:', X_test.shape)
 
-# Train the model
 
+
+#####
+# Train the model
+#####
 #set features
 features = ['gender','age','hypertension','heart_disease','smoking_history','bmi','HbA1c_level','blood_glucose_level']
 
 
 
+#####
 ### Decision Tree Classifier ###
+#####
 
 #set depth limit
 depth_limit = 8
-
 #create a model
 model = DecisionTreeClassifier(criterion= 'entropy',max_depth=depth_limit)
 #fit the model
 model.fit(X_train[features],y_train)
-
 #predict on train data 
 dt_y_pred_train = model.predict(X_train[features])
-
 #predict on the validation data
 dt_y_pred_val = model.predict(X_val[features])
 dt_y_pred_test = model.predict(X_test[features])
-
-
 #Evaluate the train model
 dt_train_accuracy = metrics.accuracy_score(y_train,dt_y_pred_train)
 dt_train_f1 = metrics.f1_score(y_train, dt_y_pred_train)
-
-
 #Evaluate the test model
 dt_val_accuracy = metrics.accuracy_score(y_val,dt_y_pred_val)
 dt_val_f1 = metrics.f1_score(y_val,dt_y_pred_val)
-
 #Evaluate the test model
 dt_test_accuracy = metrics.accuracy_score(y_test,dt_y_pred_test)
 dt_test_f1 = metrics.f1_score(y_test,dt_y_pred_test)
-
-
-
+#performance
 printTestPerformance(dt_val_accuracy, dt_val_f1, dt_test_accuracy, dt_test_f1)
-
-
-
 # Predict probabilities for the test set
 val_prob = model.predict_proba(X_val[features])[:, 1]  # Probabilities for the positive class
-
 fpr, tpr, thresholds = metrics.roc_curve(y_val, val_prob)
 roc_auc = metrics.roc_auc_score(y_val, val_prob)
 
@@ -130,34 +120,28 @@ plt.show()
 '''
 
 
-
+#####
 ### KNN ###
+#####
 
 knn_model = KNeighborsClassifier(n_neighbors=7)
 knn_model.fit(X_train, y_train)
-
 # Predict on train and validation data
 knn_y_pred_train = knn_model.predict(X_train)
 knn_y_pred_val = knn_model.predict(X_val)
-
 # Evaluate the train model
 knn_train_accuracy = metrics.accuracy_score(y_train, knn_y_pred_train)
 knn_train_f1 = metrics.f1_score(y_train, knn_y_pred_train)
-
 # Evaluate the validation model
 knn_val_accuracy = metrics.accuracy_score(y_val, knn_y_pred_val)
 knn_val_f1 = metrics.f1_score(y_val, knn_y_pred_val)
-
 # Print KNN performance
 print("")
 print("")
 print("----KNN Performance----")
 printPerformance(knn_train_accuracy, knn_train_f1, knn_val_accuracy, knn_val_f1)
-
-
 # Predict probabilities for the validation set
 knn_val_prob = knn_model.predict_proba(X_val)[:, 1]  # Probabilities for the positive class
-
 fpr_knn, tpr_knn, thresholds_nb = metrics.roc_curve(y_val, knn_val_prob)
 roc_auc_knn = metrics.roc_auc_score(y_val, knn_val_prob)
 '''
@@ -203,32 +187,28 @@ plt.tight_layout()
 plt.show()
 '''
 
+#####
 ### Naive Bayes Classifier ###
+#####
 
 # Initialize and train the model
 NB = GaussianNB()
 NB.fit(X_train, y_train)
-
 # Make predictions
 nb_y_pred_train = NB.predict(X_train)
 nb_y_pred_val = NB.predict(X_val)
-
 # Evaluate the train model
 nb_train_accuracy = metrics.accuracy_score(y_train, nb_y_pred_train)
 nb_train_f1 = metrics.f1_score(y_train, nb_y_pred_train)
-
 # Evaluate the validation model
 nb_val_accuracy = metrics.accuracy_score(y_val, nb_y_pred_val)
 nb_val_f1 = metrics.f1_score(y_val, nb_y_pred_val)
-
 print("")
 print("")
 print("----NB Performance----")
 printPerformance(nb_train_accuracy, nb_train_f1, nb_val_accuracy, nb_val_f1)
-
 # Predict probabilities for the validation set
 nb_val_prob = NB.predict_proba(X_val)[:, 1]  # Probabilities for the positive class
-
 # Compute ROC-AUC and plot ROC curve for Naive Bayes
 fpr_nb, tpr_nb, thresholds_nb = metrics.roc_curve(y_val, nb_val_prob)
 roc_auc_nb = metrics.roc_auc_score(y_val, nb_val_prob)
@@ -280,6 +260,13 @@ plt.grid()
 plt.tight_layout()
 plt.show()
 '''
+
+
+
+#####
+##### BIG GRAPH #####
+#####
+
 '''
 # Create a new figure for the combined ROC curve
 plt.figure(figsize=(10, 8))
